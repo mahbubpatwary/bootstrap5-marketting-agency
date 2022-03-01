efa1187f201f4d6785d768b01a2c52844c21b75b
mahbubpatwary <mmahbub727@gmail.com>
2022-03-01 20:19:42 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+//header scroll add 
+
+
+let nav = document.querySelector('.navbar');
+
+window.onscroll = function (params) {
+    
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add('header-scrolled');
+    }else{
+        nav.classList.remove('header-scrolled')
+    }
+}
+
+
+
+
+
+
+
+// owl carousel ??
+
+$(document).ready(function () {
+    $('.client-slider-section').owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive : {
+            0:{
+                items:2
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items : 6
+            }
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
